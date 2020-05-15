a82aa211f32853af02596560e247ff34c04282d1
Sajida Qadomi <sajidaqadomi@outlook.sa>
2020-05-15 20:52:37 +0300

    temp trial v3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 
-window.onscroll = function() {
-    document.getElementById("upper-header").style.display = "none";
+// window.onscroll = function() {
+//     document.getElementById("upper-header").style.display = "none";
     
   
 
+// }
+
+let upperHeader= document.getElementById('upper-header');
+ let lowerHeader=document.getElementById('lower-header');
+
+window.onscroll=function(){
+    'use strict';
+    
+    if(window.pageYOffset>=1){
+        upperHeader.style.display='none';
+        lowerHeader.classList.add('white-nav');
+    }else{
+        upperHeader.style.display='block';
+        lowerHeader.classList.remove('white-nav');
+
+        
+    }
 }
+
